11f98b9dd63e8bcd9cfc436c01db88ec040f9470
Johan Nordström <johan@digitalvillage.se>
2022-02-16 10:46:49 +0100

    Improved layout of todo index.

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -3,21 +3,37 @@
 @section('content')
 	<h1>Todos</h1>
 
-	<p>Warning! Here be todos!</p>
-
 	@if(count($todos) > 0)
-		<ol>
-			@foreach($todos as $todo)
-				<li>
-					<a href="/todos/{{ $todo->id }}">
-						{{ $todo->title }}
-					</a>
-					@if($todo->completed)
-						🥳
-					@endif
-				</li>
-			@endforeach
-		</ol>
+		<table class="table table-striped table-hover table-responsive">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Title</th>
+					<th>Completed</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($todos as $todo)
+					<tr>
+						<td>{{ $todo->id }}</td>
+						<td>{{ $todo->title }}</td>
+						<td>
+							@if($todo->completed)
+								🥳
+							@else
+								🥵
+							@endif
+						</td>
+						<td>
+							<a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm">
+								View
+							</a>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
 	@else
 		<p>No todos left! 🥳</p>
 	@endif
